178b27c45afaea48b7789593993a77da0b54c718
Ruby11692046 <88955684+Ruby11692046@users.noreply.github.com>
2023-05-25 17:24:33 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const InputValue = document.getElementById('password');// パスワードデータ受け取り
+const OpenButton = document.getElementById('psbtn') //送信受け取り
+
+OpenButton.onclick = () => {
+//ホームページを開く
+    location.href = password.value + '_home.html'
+}
